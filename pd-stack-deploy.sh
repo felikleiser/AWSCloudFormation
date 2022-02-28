a7a252d7e36a5585cde54a014a51c2fd45a8ca51
Felipe <felikleiser@gmail.com>
2022-02-28 18:44:41 -0500

    primer commit

---
+++ pd-stack-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+STACK_NAME=$1
+
+if [ -z "$1" ]
+  then
+    echo "No STACK_NAME argument supplied"
+    exit 1
+fi
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Creating stack..."
+aws cloudformation create-stack \
+  --stack-name ${STACK_NAME} \
+  --template-body file://${DIR}/PresidioTemplate.yml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters file://${DIR}/PresidioParameters.json \
+  --tags file://${DIR}/PresidioTags.json \
